061084ad6feacd99771f84da84dd865ad789ec95
OrlandoTele96 <orlandoglezmusic@gmail.com>
2020-11-25 19:09:34 -0600

    modified:   sybil/Node.cpp 	modified:   sybil/Node.hpp 	modified:   sybil/sybil.ino

--- sybil/Node.cpp
+++ sybil/Node.cpp
@@ -206,8 +206,8 @@ int Node::Discard()
         {
           if(id_test.at(i).ID!=id_test.at(j).ID)
           {
-            inf = id_test.at(i).prom-(0.5*(id_test.at(i).desv));
-            sup =  id_test.at(i).prom+(0.5*(id_test.at(i).desv));
+            inf = id_test.at(i).prom-(0.25*(id_test.at(i).desv));
+            sup =  id_test.at(i).prom+(0.25*(id_test.at(i).desv));
             if(id_test.at(j).prom>inf && id_test.at(j).prom<sup)
             {
               suspected.push_back(id_test.at(j).ID);
@@ -306,20 +306,21 @@ string Node::ProofOfWork(string input,int dif)
   string hash="";
   //string solution;
   to_hash = toHash(input,hash);
-  target = GenerateTarget(dif);
+  target = GenerateTarget(dif)+"b";
   do
   {
     hash = sha256(to_hash);
     to_hash = toHash(to_hash,hash);
-  }while(hash.substr(0,dif)!=target);
+  }while(hash.substr(0,dif+1)!=target);
   //cout<<"Mined"<<endl;
   return hash;
 }
 string Node::toHash(string input,string lhash)
 {
   string to_hash;
-  string key = "#Telecom1234567";
-  to_hash = input+key +lhash.substr(0,5);
+  string key = "#Telecom";
+  //input.substr(0,3)+
+  to_hash = lhash.substr(0,3)+input;
   return to_hash;
 }
 string Node::GenerateTarget(int difficulty)
@@ -390,7 +391,7 @@ int Node::SybilDetection()
     sup=this->pow_ti.at(i)+thresh_t;
     inf=this->pow_ti.at(i)-thresh_t;
 
-    
+
     for (j=0;j<this->pow_ans.size();j++)
     {
       if(pow.at(i)==pow_ans.at(j))
@@ -416,7 +417,7 @@ int Node::SybilDetection()
   this->pow_ti.clear();
   this->pow_ans.clear();
   this->pow_tf.clear();
-  this->id_tested.clear();    
+  this->id_tested.clear();
   }
     return 0;
 }
--- sybil/sybil.ino
+++ sybil/sybil.ino
@@ -12,6 +12,8 @@ int counter[4]={0,0,0,0};
 int isPow=0;
 unsigned char type=0x00;
 unsigned char dst;
+vector<vector<char>> proofs;
+vector<char> tested;
 void setup() {
   // put your setup code here, to run once:
     Heltec.begin(true, true, true, true , BAND);
@@ -27,17 +29,22 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned char type = 0x00;
+  int i;
   n.setTm(type);
   for(c=0;c<4;c++)
   {
     if (millis() - lastSendTime > interval)
     {
-       // send a message
-      //if(counter[c]<=100000)
-      //{
+      int tam = proofs.size();
+      if(tam>0)
+      {
+        for(i=0;i<tam;i++)
+        {
+          Serial.println("ID + "+String(tested.at(i)+"Proof"));
+        }
+      }
         n.setID(sybil[c]);
         sendMessage(n);
-      //}
       lastSendTime = millis();
       interval = random(500);
       LoRa.receive();
@@ -71,52 +78,22 @@ void onReceive(int packetSize)
 {
   if (packetSize == 0) return;
   Serial.print("Message Received");
-  vector<vector<char>> gl;
+  //vector<vector<char>> gl;
+  /*ID src*/
   char IDE = LoRa.read();
+  /*Type*/
   unsigned char type = LoRa.read();
+  /*ID dst*/
+  char dst = LoRa.read();
+  /*Payload*/
   String incoming="";
-  int rssi = LoRa.packetRssi();
-  Serial.println("Received from : "+String(IDE));
-  Serial.println("RSSI : "+String(rssi));
   while(LoRa.available())
   {
     incoming += (char) LoRa.read();
   }
-  Serial.println(incoming);
-  /*
-  int a =n.IsinHist(IDE);
-  if (a==1)
-  {
-    //Serial.println("This ID is in the history"+String(a));
-    if(n.isQueueFull(IDE)==true)
-    {
-      Serial.println("Queue is full, remove last rssi");
-      int r = n.RemoveRSSI(IDE);
-      Serial.println("Removed : "+String(r));
-    }
-  }
-  else
-  {
-
-    //Serial.println("This ID is not in the history, will be added"+String(a));
-    n.AddIDtoHist(IDE);
-  }
-  n.AddRSSI(IDE,rssi);
-  //Serial.println("RSSI added succesfuly");
-  int x = n.getHistSize();
-  Serial.println("Historial size = "+String(x));
-  int ans= n.Discard();
-  if(ans==1)
-  {
-    Serial.println("Gray list was made"+String(ans));
-    gl = n.getGrayList();
-    Serial.println("Graylist size()"+String(gl.size()));
-    PrintGrayList(gl);
-  }
-  else
-  {
-    Serial.println("Gray list was not made"+String(ans));
-  }*/
+  /*Read RSSI*/
+  int rssi = LoRa.packetRssi();
+  Unpack(type,dst,incoming,IDE);
 }
 
 void Unpack(unsigned char type,char i_dst,String pay,char src)
@@ -128,9 +105,71 @@ void Unpack(unsigned char type,char i_dst,String pay,char src)
   int pay_len;
   vector<char> solution;
   int pow_t;
+  vector<char> rnum;
   if(type ==0x00)
   {
     //Serial.println("Message 0 received");
+  }
+  if(type ==0x01)
+  {
+    
+    pay_len = pay.length();
+    n_id_dst = pay_len-4;
+    for(i=0;i<n_id_dst;i++)
+    {
+      if(pay.charAt(i)==sybil[0])
+      {
+        
+        Serial.println("Message 1 received");
+        rnum.push_back(pay.charAt(pay_len-4));
+        rnum.push_back(pay.charAt(pay_len-3));
+        rnum.push_back(pay.charAt(pay_len-2));
+        rnum.push_back(pay.charAt(pay_len-1));
+        proofs.push_back(rnum);
+        rnum.clear();
+        tested.push_back(sybil[0]);
+        dst=src;
+      }
+      if(pay.charAt(i)==sybil[1])
+      {
+        
+        Serial.println("Message 1 received");
+        rnum.push_back(pay.charAt(pay_len-4));
+        rnum.push_back(pay.charAt(pay_len-3));
+        rnum.push_back(pay.charAt(pay_len-2));
+        rnum.push_back(pay.charAt(pay_len-1));
+        proofs.push_back(rnum);
+        tested.push_back(sybil[1]);
+        rnum.clear();
+        dst=src;
+      }
+      if(pay.charAt(i)==sybil[2])
+      {
+        
+        Serial.println("Message 1 received");
+        rnum.push_back(pay.charAt(pay_len-4));
+        rnum.push_back(pay.charAt(pay_len-3));
+        rnum.push_back(pay.charAt(pay_len-2));
+        rnum.push_back(pay.charAt(pay_len-1));
+        proofs.push_back(rnum);
+        tested.push_back(sybil[2]);
+        rnum.clear();
+        dst=src;
+      }
+      if(pay.charAt(i)==sybil[3])
+      {
+        
+        Serial.println("Message 1 received");
+        rnum.push_back(pay.charAt(pay_len-4));
+        rnum.push_back(pay.charAt(pay_len-3));
+        rnum.push_back(pay.charAt(pay_len-2));
+        rnum.push_back(pay.charAt(pay_len-1));
+        proofs.push_back(rnum);
+        tested.push_back(sybil[3]);
+        rnum.clear();
+        dst=src;
+      }
+    }
   }
 }
 
