b044e951eb20b109adff9cfca586ba6b90e1b7ca
OrlandoTele96 <jorge.o.gonzalez.dev@gmail.com>
2020-12-17 21:16:16 -0600

    modificado:     Sender_Receiver/Node.cpp 	modificado:     src/SerialCapture/SerialCapture.py 	nuevo archivo:  src/SerialCapture/pow_node1_05_dif3.txt

--- Sender_Receiver/Node.cpp
+++ Sender_Receiver/Node.cpp
@@ -504,7 +504,7 @@ void Node::calcTmin()
       }
     }
     
-    tole =(this->time_interval+10+tmin)*0.25;
+    tole =(this->time_interval+10+tmin)*0.5;
     //cout<<tole<<endl;
     this->tol.push_back(tole);
   }
